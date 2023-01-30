f20aa5ae2e09c2120d25fb4449bf5e32545c280c
Yaroslav Horievoi <yaroslavgorevoy@gmail.com>
2023-01-30 23:41:36 +0200

    From Yaroslav

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ function changeBackground(background) {
         break;
         
         case 'service':
-            setStyles('rgba(241, 54, 54, 0.56)','360','170vh','0')
+            setStyles('rgba(241, 54, 54, 0.56)','360','170vh')
         break;
         
         case 'subscribe':
@@ -19,19 +19,20 @@ function changeBackground(background) {
         break;
         }
 }
-function setStyles(background,deg,widht,disp) {
+function setStyles(background,deg,widht) {
     let bgStyles = document.getElementById('background')
     let elmemGl = document.getElementById('gl1')
-    console.log(elmemGl)
+    bgStyles.style.display='block'
     if (!background) {
         elmemGl.style.color='black'
         bgStyles.style.borderBottom='170vh solid rgba(153, 153, 153, 0.8)'
         bgStyles.style.transform='rotate(270deg)'
+    } else if (background === 'none') {
+        bgStyles.style.display='none'
     } else {
         elmemGl.style.color='white' 
         bgStyles.style.borderBottom=`${widht} solid ${background}`
         bgStyles.style.transform=`rotate(${deg}deg)`
-        (background === 'none') ? bgStyles.style.display = 'none' : 
     }
 
 }
